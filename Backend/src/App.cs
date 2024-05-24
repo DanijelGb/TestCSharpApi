3e8cc9631113f7a050f13af28323cca9bc8a3596
DanijelGb <124356150+DanijelGb@users.noreply.github.com>
2024-05-24 08:02:49 +0200

    Deleted master and pushing over feature branch

---
+++ Backend/src/App.cs
@@ -0,0 +1,14 @@
+// Global settings
+Globals = Obj(new
+{
+    debugOn = true,
+    detailedAclDebug = false,
+    aclOn = true,
+    isSpa = true,
+    port = 3001,
+    serverName = "Dagbas API Server",
+    frontendPath = FilePath("..", "Frontend"),
+    sessionLifeTimeHours = 2
+});
+
+Server.Start();
